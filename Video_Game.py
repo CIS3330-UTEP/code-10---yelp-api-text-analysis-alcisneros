84b4dc54505e89bb9d82780c8062042e58c7e8bd
alcisneros <143829334+alcisneros@users.noreply.github.com>
2023-12-12 22:15:33 -0700

    Final project

--- Video_Game.py
+++ Video_Game.py
@@ -2,7 +2,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
+import statsmodels.api as sm
+import statsmodels.formula.api as sm_api
 import statsmodels.graphics.api as smg
+import statsmodels.stats.anova as anova
 
 df = pd.read_csv('video_games.csv')
 #print(df.describe())
@@ -23,10 +26,15 @@ df = pd.read_csv('video_games.csv')
 # plt.title('Scatter Chart')
 # plt.show()
 
-# filter_query = "'Features.Max Players' == '1' or 'Features.Max Players' == '2'"
+# filter_query = "`Features.Max Players` == 1 or `Features.Max Players` == 2 or `Features.Max Players` == 3 or `Features.Max Players` == 4 or `Features.Max Players` == 5 or `Features.Max Players` == 6 or `Features.Max Players` == 7 or `Features.Max Players` == 8"
 # players_df = df.query(filter_query)
 # players_df.boxplot(column= "Metrics.Review Score", by="Features.Max Players")
 # plt.show()
+
+# df_top3 = df.query(`Features.Max Players` == 1 or `Features.Max Players` == 2 or `Features.Max Players` == 3)
+# model = sm_api.ols('Metrics.Review Score ~ C(Features.Max Players)', data=df_top3).fit()
+# anova_table = sm.stats.anova_lm(model, typ=2)
+# print(anova_table)
 
 # df_num = df.select_dtypes(['number'])
 # game_corr_matrix = np.corrcoef(df_num.T)
@@ -35,18 +43,31 @@ df = pd.read_csv('video_games.csv')
 # sns.heatmap(corr_matrix, annot=True, vmax=1, vmin=-1, cmap='coolwarm')
 # plt.show()
 
-categories = (df['Features.Online?'])
-values =(df['Length.All PlayStyles.Average'])
-plt.bar(categories, values)
-plt.xlabel('Online T or F')
-plt.ylabel('Length')
-plt.title('Bar Chart')
-plt.show()
+# categories = (df['Features.Online?'])
+# values =(df['Length.All PlayStyles.Average'])
+# plt.bar(categories, values)
+# plt.xlabel('Online T or F')
+# plt.ylabel('Length')
+# plt.title('Bar Chart')
+# plt.show()
 
-categories = (df['Features.Online?'])
-values =(df['Length.All PlayStyles.Average'])
-plt.scatter(categories, values)
-plt.xlabel('Online T or F')
-plt.ylabel('Length')
-plt.title('Scatter Chart')
-plt.show()
+# categories = (df['Features.Online?'])
+# values =(df['Length.All PlayStyles.Average'])
+# plt.scatter(categories, values)
+# plt.xlabel('Online T or F')
+# plt.ylabel('Length')
+# plt.title('Scatter Chart')
+# plt.show()
+
+# categories = (df['Features.Max Players'])
+# values =(df['Metrics.Sales'])
+# plt.bar(categories, values)
+# plt.xlabel('Number of Players')
+# plt.ylabel('sales')
+# plt.title('Bar Chart')
+# plt.show()
+
+# filter_query = "`Features.Max Players` == 1 or `Features.Max Players` == 2 or `Features.Max Players` == 3 or `Features.Max Players` == 4 or `Features.Max Players` == 5 or `Features.Max Players` == 6 or `Features.Max Players` == 7 or `Features.Max Players` == 8"
+# players_df = df.query(filter_query)
+# players_df.boxplot(column= "Metrics.Used Price", by="Features.Max Players")
+# plt.show()
